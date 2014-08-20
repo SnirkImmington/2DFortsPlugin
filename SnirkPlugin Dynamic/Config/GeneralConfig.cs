a56ffff9a410284157783b3c6685a2d1ba8545cc
Snirk Immington <snirk.immington@gmail.com>
2014-08-20 16:07:31 +1000

    Commenting the configuration file

--- SnirkPlugin Dynamic/Config/GeneralConfig.cs
+++ SnirkPlugin Dynamic/Config/GeneralConfig.cs
@@ -6,12 +6,32 @@ using System.Threading.Tasks;
 
 namespace SnirkPlugin_Dynamic
 {
+    /// <summary>
+    /// Contains general configurations.
+    /// </summary>
     class GeneralConfig
     {
+        /// <summary>
+        /// The basic commands that are allowed if someone is prevented from using commands.
+        /// </summary>
         public string[] AllowedCommands;
 
+        /// <summary>
+        /// The standard color for plugin logs.
+        /// </summary>
         public static Color StandardLogsColor = Color.MediumAquamarine;
+        /// <summary>
+        /// The standard color for plugin tracing.
+        /// </summary>
         public static Color TraceLogsColor = Color.MistyRose;
+        /// <summary>
+        /// The standard log for errors.
+        /// </summary>
         public static Color ErrorLogsColor = Color.Red;
+
+        public static void Init(bool first)
+        {
+
+        }
     }
 }
