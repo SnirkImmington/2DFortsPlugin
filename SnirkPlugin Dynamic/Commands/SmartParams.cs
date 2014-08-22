238de5e896515744435307ca37cb9ddccd80ecfd
Snirk Immington <snirk.immington@gmail.com>
2014-08-22 18:25:40 +1000

    Added "SmartParams" - commands with players/mobs don't need spaces

---
+++ SnirkPlugin Dynamic/Commands/SmartParams.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TShockAPI;
+
+namespace SnirkPlugin_Dynamic
+{
+    static class SmartParams
+    {
+        public static SmartInfo<PlayerData> Player(CommandArgs com, int startIndex = 0)
+        {
+
+        }
+    }
+
+    class SmartInfo<T>
+    {
+        public T Value;
+
+        public int Index;
+
+        public SmartInfo(T value, int index)
+        {
+            Value = value; Index = index;
+        }
+    }
+}
