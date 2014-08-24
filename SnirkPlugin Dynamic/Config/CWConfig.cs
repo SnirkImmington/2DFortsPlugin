ee7c7269374893fd423bd472c1351bda090ef2a1
Snirk Immington <snirk.immington@gmail.com>
2014-08-24 23:14:45 +1000

    Added CWConfig

---
+++ SnirkPlugin Dynamic/Config/CWConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnirkPlugin_Dynamic
+{
+    /// <summary>
+    /// Contains configuration for class warfare.
+    /// </summary>
+    static class CWConfig
+    {
+        /// <summary>
+        /// The color of the messages to be sent to players.
+        /// </summary>
+        public static Color MessageColor = Color.Firebrick;
+        /// <summary>
+        /// The color of all blue team messages.
+        /// </summary>
+        public static Color BlueTeamColor = Color.SkyBlue;
+        /// <summary>
+        /// The color of all red team messages.
+        /// </summary>
+        public static Color RedTeamColor = Color.OrangeRed;
+    }
+}
