fbc2a15f1b3025940b421b58ca33f0eda18e64d9
Snirk Immington <snirk.immington@gmail.com>
2014-08-24 16:54:13 +1000

    Added statistics data

--- SnirkPlugin Dynamic/CW/CWGameStats.cs
+++ SnirkPlugin Dynamic/CW/CWGameStats.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace SnirkPlugin_Dynamic
 {
+    /// <summary>
+    /// Statistics for CWGames
+    /// </summary>
     class CWGameStats
     {
+        /// <summary>
+        /// Bool to store winning team.
+        /// </summary>
         public bool RedWon { get; private set; }
+        /// <summary>
+        /// How much the other team mined (winner mine can be determined).
+        /// </summary>
         public int OtherTeamMined { get; private set; }
+        /// <summary>
+        /// The arena the game was played at.
+        /// </summary>
         public CWArena ArenaPlayed { get; private set; }
+        /// <summary>
+        /// When the game started (was played).
+        /// </summary>
+        public DateTime StartTime { get; private set; }
+        /// <summary>
+        /// When the game ended (length).
+        /// </summary>
+        public DateTime EndTime { get; private set; }
     }
 }
