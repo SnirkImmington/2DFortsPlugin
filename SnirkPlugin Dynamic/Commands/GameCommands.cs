dd0695f0afb6e74e921ef4194335013857f13a48
Snirk Immington <snirk.immington@gmail.com>
2014-08-26 16:07:45 +1000

    Added subcommands to /cw

--- SnirkPlugin Dynamic/Commands/GameCommands.cs
+++ SnirkPlugin Dynamic/Commands/GameCommands.cs
@@ -17,8 +17,9 @@ namespace SnirkPlugin_Dynamic
         public static void CW(CommandArgs com)
         {
             // Usage:
-            // cw host
-            // cw join
+            // ! cw host
+            // cw join [arena]
+            // cw listen [arena]
             // cw observe [arena]
             // cw continue <arena>
             // cw pause
@@ -26,6 +27,9 @@ namespace SnirkPlugin_Dynamic
             // cw stop
             // cw kick <player> (smart)
             // cw refill
+            // cw classinfo <class> - stats purposes
+            // cw confirm (optional)
+            // cw who|listplayers
 
             // Create the parser with the usage.
             var parser = new CommandParser(com, GetCWUsage(com.FPlayer()));
@@ -34,6 +38,7 @@ namespace SnirkPlugin_Dynamic
             var player = com.FPlayer();
             switch (type)
             {
+                #region cw host
                 case "host":
                 case "start":
                     if (!com.FPlayer().IsDonor || !com.Player.IsStaff())
@@ -44,11 +49,17 @@ namespace SnirkPlugin_Dynamic
                     {
                         com.Player.SendErrorMessage("You are already playing CW!"); return;
                     }
+                    if (DynamicMain.CWGames.Any(g => g.State < CWGameState.Playing))
+                    {
+                        com.Player.SendErrorMessage("There is already a game being prepared! Join that one or wait!"); return;
+                    }
                     var game = new CWGame(player);
                     com.Player.SendInfoMessage("Game started. Remember, you still need /cw join to play! You may leave and come back with a new character if you need.");
-                    TSPlayer.All.SendMessage("has hosted a new game of Class Warfare!", );
+                    TSPlayer.All.SendMessage("{0} has hosted a new game of Class Warfare! Type /cw join to join.", CWConfig.MessageColor);
                     return;
+                #endregion
 
+                #region cw join
                 case "join":
                     if (player.CW != null)
                     {
@@ -60,31 +71,76 @@ namespace SnirkPlugin_Dynamic
                     {
                         com.Player.SendErrorMessage("There isn't a CW game to join right now!"); return;
                     }
+                    else if (games.Count != 1)
+                    {
+                        com.Player.SendErrorMessage("There are more than one CW game preparing! This is not a state the plugin was supposed to be in!");
+                        // TODO should it try to fix that or assume it's an exception?
+                        return;
+                    }
+                    // TODO Determine if player is eligible!!!
+                    com.Player.Teleport();
+                    com.Player.SendSuccessMessage("Please stand on a");
                     return;
+                #endregion
 
+                #region cw observe
                 case "observe":
                 case "spectate":
+                case "watch":
                     return;
+                #endregion
 
+                #region cw classinfo
+                case "classinfo":
+                case "class":
+                case "classstats":
+                    return;
+                #endregion
+
+                #region cw confirm - optional
+                case "confirm":
+                case "chooseclass":
+                    return;
+                #endregion
+
+                #region cw listplayers
+                case "who":
+                case "list":
+                case "listplayers":
+                case "players":
+                case "playing":
+                case "online":
+                    return;
+                #endregion
+
+                #region cw continue
                 case "continue":
+                case "resume":
+                    // Merge /cw continue and /cw resume
                     return;
+                #endregion
 
+                #region cw pause
                 case "pause":
                     return;
+                #endregion
 
-                case "resume":
-                    return;
-
+                #region cw stop
                 case "stop":
                 case "halt":
                     return;
+                #endregion
 
+                #region cw refill
                 case "refill":
                     return;
+                #endregion
 
+                #region cw kick
                 case "kick":
                 case "ban":
                     return;
+                #endregion
             }
         }
 
@@ -128,5 +184,7 @@ namespace SnirkPlugin_Dynamic
             builder.Append(" - Class Warfare command."); 
             return builder.ToString();
         }
+
+
     }
 }
